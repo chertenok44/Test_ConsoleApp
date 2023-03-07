ce48847cb0b6a729fbc6036c0019dca2b126e81c
Vasily Ustugov <Vasily.Ustugov@x-tensive.com>
2023-03-07 18:56:29 +0500

    fix output

--- HelloWorld_ConsoleApp/Program.cs
+++ HelloWorld_ConsoleApp/Program.cs
@@ -12,6 +12,7 @@
 			Console.WriteLine("another one part of text");
 			Console.WriteLine("i hope it is last part of text");
 			Console.WriteLine("is it last attempt?");
+			Console.WriteLine("I will try again");
 		}
 	}
 }
