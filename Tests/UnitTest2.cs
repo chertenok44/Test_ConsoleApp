2e35437c0c34d02865051d223b0bd33aa71c46ad
Vasily Ustugov <Vasily.Ustugov@x-tensive.com>
2023-03-07 13:54:09 +0500

    add tests (part2)

---
+++ Tests/UnitTest2.cs
@@ -0,0 +1,41 @@
+namespace Tests
+{
+	[TestFixture(Category = "Simple tests (Part2)")]
+	public class TestsPart2
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test11()
+		{
+			Assert.Pass();
+		}
+
+		[Test]
+		public void Test12()
+		{
+			Assert.Pass();
+		}
+
+		[Test]
+		public void Test13()
+		{
+			Assert.Pass();
+		}
+
+		[Test]
+		public void Test14()
+		{
+			Assert.Pass();
+		}
+
+		[Test]
+		public void Test15()
+		{
+			Assert.Pass();
+		}
+	}
+}
